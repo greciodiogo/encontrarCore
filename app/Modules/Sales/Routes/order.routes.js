4cfde9de2fe4ff77a78bf3affa29212a49927455
Grécio Diogo <fonebahia8@gmail.com>
2025-12-20 17:57:17 +0100

    feat: add manager roles

--- app/Modules/Sales/Routes/order.routes.js
+++ app/Modules/Sales/Routes/order.routes.js
@@ -2,10 +2,10 @@
     module.exports = (ApiRoute, Route) => 
     // Protected routes
     ApiRoute(() => {
-      Route.get("/", "OrderController.index").middleware(['role:admin']);
-      Route.post("/", "OrderController.store").middleware(['role:admin']);
-      Route.get("/:id", "OrderController.show").middleware(['role:admin']);
-      Route.put("/:id", "OrderController.update").middleware(['role:admin']);
-      Route.delete("/:id", "OrderController.destroy").middleware(['role:admin']);
+      Route.get("/", "OrderController.index").middleware(['role:admin, manager']);
+      Route.post("/", "OrderController.store").middleware(['role:admin, manager, customer']);
+      Route.get("/:id", "OrderController.show").middleware(['role:admin, manager']);
+      Route.put("/:id", "OrderController.update").middleware(['role:admin, manager']);
+      Route.delete("/:id", "OrderController.destroy").middleware(['role:admin, manager']);
     }, 'orders').namespace("App/Modules/Sales/Controllers").middleware(["auth"]);
     
