4ae4b6a2f4b26c04166c6cd37673483196f0dae6
Grécio Diogo <fonebahia8@gmail.com>
2025-12-23 18:48:09 +0100

    fix: remove role in order post method

--- app/Modules/Sales/Routes/order.routes.js
+++ app/Modules/Sales/Routes/order.routes.js
@@ -3,9 +3,9 @@
     // Protected routes
     ApiRoute(() => {
       Route.get("/", "OrderController.index").middleware(['role:admin,manager']);
-      Route.post("/", "OrderController.store").middleware(['role:admin,customer,manager']);
+      Route.post("/", "OrderController.store");
       Route.get("/:id", "OrderController.show").middleware(['role:admin,customer,manager']);
       Route.put("/:id", "OrderController.update").middleware(['role:admin,manager']);
       Route.delete("/:id", "OrderController.destroy").middleware(['role:admin,manager']);
-    }, 'orders').namespace("App/Modules/Sales/Controllers").middleware(["auth"]);
+    }, 'orders').namespace("App/Modules/Sales/Controllers")
     
