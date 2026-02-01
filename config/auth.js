75f2011718039786a3581ea21ae0ebe98863f930
Grécio Diogo <fonebahia8@gmail.com>
2026-02-01 05:29:49 +0100

    feat: increase jwt token lifetime

--- config/auth.js
+++ config/auth.js
@@ -73,7 +73,7 @@ module.exports = {
     password: 'password',
     options: {
       secret: Env.get('APP_KEY'),
-      expiresIn: '10h',
+      expiresIn: '30d', // 30 dias para apps mobile
     }
   },
 
