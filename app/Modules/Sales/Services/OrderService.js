276bb02ac918bf826f15b0f6aae2b008381fef85
Grécio Diogo <fonebahia8@gmail.com>
2026-02-12 14:58:42 +0100

    fix: fix last commit

--- app/Modules/Sales/Services/OrderService.js
+++ app/Modules/Sales/Services/OrderService.js
@@ -49,7 +49,7 @@ const FirebaseService = use('App/Services/FirebaseService')
           'orders.fullName as client',
           'orders.contactPhone',
           'orders.contactEmail',
-          'orders.total',
+          'orders.total_amount as total',
           'payment_methods.name as payment_method',
           'order_deliveries.address as delivery_address',
           'order_deliveries.price as delivery_tax'
